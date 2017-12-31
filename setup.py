58459e5c7d7c061af420cd4b1a1860e012625f3c
ptbrodie <patrick@findsignal.co>
2017-12-31 18:51:48 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pycoins',
-      version='0.0.2',
+      version='0.0.3',
       description='A little command line tool for tracking cryptocurrency prices.',
       url='http://github.com/ptbrodie/coins',
       author='Patrick Brodie',
