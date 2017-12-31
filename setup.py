33243485120dde027a1cec1c52ec3fc5f53402dd
ptbrodie <patrick@findsignal.co>
2017-12-31 18:51:27 -0500

    add readme

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
-setup(name='coins',
-      version='0.0.1',
+setup(name='pycoins',
+      version='0.0.2',
       description='A little command line tool for tracking cryptocurrency prices.',
       url='http://github.com/ptbrodie/coins',
       author='Patrick Brodie',
+      author_email='ptbrodie@gmail.com',
       long_description=open('README.md').read(),
       install_requires=[
           'requests==2.18.4',
           'termcolor==1.1.0'
       ],
-      scripts=['bin/coins'])
+      scripts=['./coins'])
