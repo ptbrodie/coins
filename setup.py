d844d096385e38bae0074a720b8b9fc24e60aac7
ptbrodie <patrick@findsignal.co>
2017-12-31 19:31:57 -0500

    remove readme description

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='pycoins',
       url='http://github.com/ptbrodie/coins',
       author='Patrick Brodie',
       author_email='ptbrodie@gmail.com',
-      long_description=open('./README.md').read(),
       install_requires=[
           'requests==2.18.4',
           'termcolor==1.1.0'
